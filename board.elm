6b5315662e97a38f6a99426877b0b7e478e64051
Jose Falcon <falcon@spotify.com>
2016-08-28 13:02:13 -0400

    Add clue numbers - very ugly

--- board.elm
+++ board.elm
@@ -31,12 +31,14 @@ type alias Answer =
   , clue : String
   , answer : String
   , direction : Direction
+  , number : Int
   }
 
 -- decoders
 example = """
-{"size":{"width":9,"height":9},"answers":[{"locations":[[0,0],[0,1],[0,2],[0,3]],"clue":"Vegetarian option","answer":"tofu","direction":"across"},{"locations":[[0,5],[0,6],[0,7],[0,8]],"clue":"Item on a desktop","answer":"icon","direction":"across"},{"locations":[[1,0],[1,1],[1,2],[1,3]],"clue":"Instrument on Guinness logo","answer":"harp","direction":"across"},{"locations":[[1,5],[1,6],[1,7],[1,8]],"clue":"Nickname for Jose","answer":"pepe","direction":"across"},{"locations":[[2,0],[2,1],[2,2],[2,3]],"clue":"'Ahh, makes sense'","answer":"isee","direction":"across"},{"locations":[[2,5],[2,6],[2,7],[2,8]],"clue":"Et __ (and others)","answer":"alia","direction":"across"},{"locations":[[3,0],[3,1],[3,2],[3,3],[3,4]],"clue":"Two column notebook","answer":"steno","direction":"across"},{"locations":[[3,6],[3,7],[3,8]],"clue":"Suffix for auction and mountain","answer":"eer","direction":"across"},{"locations":[[4,2],[4,3],[4,4],[4,5],[4,6]],"clue":"Extemporize","answer":"adlib","direction":"across"},{"locations":[[5,0],[5,1],[5,2]],"clue":"Swing side to side","answer":"wag","direction":"across"},{"locations":[[5,4],[5,5],[5,6],[5,7],[5,8]],"clue":"Energy scandal from 2001","answer":"enron","direction":"across"},{"locations":[[6,0],[6,1],[6,2],[6,3]],"clue":"Scores perfectly","answer":"aces","direction":"across"},{"locations":[[6,5],[6,6],[6,7],[6,8]],"clue":"Famous lamp of the 1960's","answer":"lava","direction":"across"},{"locations":[[7,0],[7,1],[7,2],[7,3]],"clue":"Row on a chessboard","answer":"rank","direction":"across"},{"locations":[[7,5],[7,6],[7,7],[7,8]],"clue":"'Up and ___'","answer":"atem","direction":"across"},{"locations":[[8,0],[8,1],[8,2],[8,3]],"clue":"Regret or disappointment","answer":"pity","direction":"across"},{"locations":[[8,5],[8,6],[8,7],[8,8]],"clue":"Used to be","answer":"were","direction":"across"},{"locations":[[0,0],[1,0],[2,0],[3,0]],"clue":"Not that","answer":"this","direction":"down"},{"locations":[[5,0],[6,0],[7,0],[8,0]],"clue":"Drive for faster than light travel","answer":"warp","direction":"down"},{"locations":[[0,1],[1,1],[2,1],[3,1]],"clue":"Where hops are dried","answer":"oast","direction":"down"},{"locations":[[5,1],[6,1],[7,1],[8,1]],"clue":"Antioxidant rich superfood","answer":"acai","direction":"down"},{"locations":[[0,2],[1,2],[2,2],[3,2],[4,2],[5,2],[6,2],[7,2],[8,2]],"clue":"Professional athlete lacking a contract","answer":"freeagent","direction":"down"},{"locations":[[0,3],[1,3],[2,3],[3,3],[4,3]],"clue":"Turn over","answer":"upend","direction":"down"},{"locations":[[6,3],[7,3],[8,3]],"clue":"Lucy's locale","answer":"sky","direction":"down"},{"locations":[[3,4],[4,4],[5,4]],"clue":"Grand ___ Opry","answer":"ole","direction":"down"},{"locations":[[0,5],[1,5],[2,5]],"clue":"Bar order, briefly","answer":"ipa","direction":"down"},{"locations":[[4,5],[5,5],[6,5],[7,5],[8,5]],"clue":"Marital kinship","answer":"inlaw","direction":"down"},{"locations":[[0,6],[1,6],[2,6],[3,6],[4,6],[5,6],[6,6],[7,6],[8,6]],"clue":"Kool & the Gang album from 1980","answer":"celebrate","direction":"down"},{"locations":[[0,7],[1,7],[2,7],[3,7]],"clue":"Ron Howard in The Andy Griffith Show","answer":"opie","direction":"down"},{"locations":[[5,7],[6,7],[7,7],[8,7]],"clue":"End of message on the radio","answer":"over","direction":"down"},{"locations":[[0,8],[1,8],[2,8],[3,8]],"clue":"Close","answer":"near","direction":"down"},{"locations":[[5,8],[6,8],[7,8],[8,8]],"clue":"Appoint (someone)","answer":"name","direction":"down"}]}
+{"size":{"width":9,"height":9},"answers":[{"locations":[[0,0],[0,1],[0,2],[0,3]],"clue":"Vegetarian option","answer":"tofu","direction":"across","number":1},{"locations":[[0,0],[1,0],[2,0],[3,0]],"clue":"Not that","answer":"this","direction":"down","number":1},{"locations":[[0,1],[1,1],[2,1],[3,1]],"clue":"Where hops are dried","answer":"oast","direction":"down","number":2},{"locations":[[0,2],[1,2],[2,2],[3,2],[4,2],[5,2],[6,2],[7,2],[8,2]],"clue":"Professional athlete lacking a contract","answer":"freeagent","direction":"down","number":3},{"locations":[[0,3],[1,3],[2,3],[3,3],[4,3]],"clue":"Turn over","answer":"upend","direction":"down","number":4},{"locations":[[0,5],[0,6],[0,7],[0,8]],"clue":"Item on a desktop","answer":"icon","direction":"across","number":5},{"locations":[[0,5],[1,5],[2,5]],"clue":"Bar order, briefly","answer":"ipa","direction":"down","number":5},{"locations":[[0,6],[1,6],[2,6],[3,6],[4,6],[5,6],[6,6],[7,6],[8,6]],"clue":"Kool & the Gang album from 1980","answer":"celebrate","direction":"down","number":6},{"locations":[[0,7],[1,7],[2,7],[3,7]],"clue":"Ron Howard in The Andy Griffith Show","answer":"opie","direction":"down","number":7},{"locations":[[0,8],[1,8],[2,8],[3,8]],"clue":"Close","answer":"near","direction":"down","number":8},{"locations":[[1,0],[1,1],[1,2],[1,3]],"clue":"Instrument on Guinness logo","answer":"harp","direction":"across","number":9},{"locations":[[1,5],[1,6],[1,7],[1,8]],"clue":"Nickname for Jose","answer":"pepe","direction":"across","number":10},{"locations":[[2,0],[2,1],[2,2],[2,3]],"clue":"'Ahh, makes sense'","answer":"isee","direction":"across","number":11},{"locations":[[2,5],[2,6],[2,7],[2,8]],"clue":"Et __ (and others)","answer":"alia","direction":"across","number":12},{"locations":[[3,0],[3,1],[3,2],[3,3],[3,4]],"clue":"Two column notebook","answer":"steno","direction":"across","number":13},{"locations":[[3,4],[4,4],[5,4]],"clue":"Grand ___ Opry","answer":"ole","direction":"down","number":14},{"locations":[[3,6],[3,7],[3,8]],"clue":"Suffix for auction and mountain","answer":"eer","direction":"across","number":15},{"locations":[[4,2],[4,3],[4,4],[4,5],[4,6]],"clue":"Extemporize","answer":"adlib","direction":"across","number":16},{"locations":[[4,5],[5,5],[6,5],[7,5],[8,5]],"clue":"Marital kinship","answer":"inlaw","direction":"down","number":17},{"locations":[[5,0],[5,1],[5,2]],"clue":"Swing side to side","answer":"wag","direction":"across","number":18},{"locations":[[5,0],[6,0],[7,0],[8,0]],"clue":"Drive for faster than light travel","answer":"warp","direction":"down","number":18},{"locations":[[5,1],[6,1],[7,1],[8,1]],"clue":"Antioxidant rich superfood","answer":"acai","direction":"down","number":19},{"locations":[[5,4],[5,5],[5,6],[5,7],[5,8]],"clue":"Energy scandal from 2001","answer":"enron","direction":"across","number":20},{"locations":[[5,7],[6,7],[7,7],[8,7]],"clue":"End of message on the radio","answer":"over","direction":"down","number":21},{"locations":[[5,8],[6,8],[7,8],[8,8]],"clue":"Appoint (someone)","answer":"name","direction":"down","number":22},{"locations":[[6,0],[6,1],[6,2],[6,3]],"clue":"Scores perfectly","answer":"aces","direction":"across","number":23},{"locations":[[6,3],[7,3],[8,3]],"clue":"Lucy's locale","answer":"sky","direction":"down","number":24},{"locations":[[6,5],[6,6],[6,7],[6,8]],"clue":"Famous lamp of the 1960's","answer":"lava","direction":"across","number":25},{"locations":[[7,0],[7,1],[7,2],[7,3]],"clue":"Row on a chessboard","answer":"rank","direction":"across","number":26},{"locations":[[7,5],[7,6],[7,7],[7,8]],"clue":"'Up and ___'","answer":"atem","direction":"across","number":27},{"locations":[[8,0],[8,1],[8,2],[8,3]],"clue":"Regret or disappointment","answer":"pity","direction":"across","number":28},{"locations":[[8,5],[8,6],[8,7],[8,8]],"clue":"Used to be","answer":"were","direction":"across","number":29}]}
 """
+
 directionSpecific : String -> Decoder Direction
 directionSpecific s =
   case s of
@@ -61,11 +63,12 @@ position =
 
 answer : Decoder Answer
 answer =
-  object4 Answer
+  object5 Answer
     ("locations" := (list position))
     ("clue" := string)
     ("answer" := string)
     ("direction" := direction)
+    ("number" := int)
 
 puzzle : Decoder Model
 puzzle =
@@ -156,29 +159,38 @@ update msg model =
     _ -> (model, Cmd.none)
 
 
+clueNumbers : Model -> Dict.Dict Position Int
+clueNumbers model =
+  (List.map (\a -> (a.locations |> List.head |> Maybe.withDefault (0, 0), a.number)) model.answers) |> Dict.fromList
+
+
 view : Model -> Html Msg
 view model =
   div [ style [ ("padding", "1rem") ] ]
-    [ viewBoard model.size model.board model.cursor
+    [ viewBoard model
     , viewClues model Across
     , viewClues model Down
     -- , text (model |> activeAnswers |> toString)
     ]
 
-viewBoard : Dimensions -> Board -> Position -> Html Msg
-viewBoard size board cursor =
+viewBoard : Model -> Html Msg
+viewBoard model =
   table [ style [("border-collapse", "collapse")] ]
-    [ tbody [] (List.map (viewRow size.width board cursor) [0..(size.height - 1)]) ]
+    [ tbody [] (List.map (viewRow model) [0..(model.size.height - 1)]) ]
 
 
-viewRow : Int -> Board -> Position -> Int -> Html Msg
-viewRow size board cursor row =
+viewRow : Model -> Int -> Html Msg
+viewRow model row =
   let
-    cell col = case (Dict.get (row, col) board) of
-      Maybe.Just c -> App.map (CellMsg (row, col)) (Cell.view c ((row, col) == cursor))
+    -- activeClues = activeAnswers model
+    -- number = if (List.any (\a -> a.locations |> head |> ((==) (row, col))) activeAnswers)
+    --          then Just
+    number col = (Dict.get (row, col) (clueNumbers model))
+    cell col = case (Dict.get (row, col) model.board) of
+      Maybe.Just c -> App.map (CellMsg (row, col)) (Cell.view c ((row, col) == model.cursor) (number col))
       Maybe.Nothing -> td [] []
   in
-    tr [] (List.map cell [0..(size - 1)])
+    tr [] (List.map cell [0..(model.size.width - 1)])
 
 
 viewClues : Model -> Direction -> Html Msg
@@ -186,11 +198,22 @@ viewClues model direction =
   let
     activeClues = activeAnswers model
     isActiveClue c = List.any ((==) c) activeClues
-    clues = model.answers |> List.filter (\s -> s.direction == direction)
+    clues = model.answers |> List.filter (\s -> s.direction == direction) |> List.sortBy .number
   in
     div [ style [("float", "left"), ("overflow", "hidden")] ]
       [ h2 [] [ text (toString direction) ]
-      , ul [] (List.map (\c -> li [ style (if (isActiveClue c) then [("background-color", "red")] else []) ] [ text c.clue ]) clues)
+      , ul [ style [("padding", "0")] ] (List.map (\c -> viewClue c (isActiveClue c)) clues)
+      ]
+
+
+viewClue clue active =
+  let
+    listStyle = [ ("list-style", "none") ]
+    highlight = if active then [("background-color", "red")] else []
+  in
+    li [ style (highlight ++ listStyle) ]
+      [ text ((toString clue.number) ++ " ")
+      , text clue.clue
       ]
 
 
--- cell.elm
+++ cell.elm
@@ -1,6 +1,6 @@
 module Cell exposing (Model(Value, Empty, Block), Msg(Click), update, view)
 
-import Html exposing (Html, td, text)
+import Html exposing (Html, td, text, span)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 import String
@@ -31,9 +31,12 @@ letterBox =
     , ("text-align", "center")
     , ("text-transform", "uppercase")
     , ("height", "2rem")
+    , ("min-height", "2rem")
     , ("width", "2rem")
+    , ("min-width", "2rem")
     , ("font-family", "monospace")
     , ("font-size", "1.5rem")
+    , ("position", "relative")
     ]
 
 
@@ -41,13 +44,26 @@ backgroundColor : String -> List (String, String)
 backgroundColor color = [ ("background-color", color) ]
 
 
-view : Model -> Bool -> Html Msg
-view model selected =
+numberStyle : List (String, String)
+numberStyle =
+  [ ("position", "absolute")
+  , ("left", "0")
+  , ("top", "0")
+  , ("font-size", ".5rem")
+  , ("line-height", "100%")
+  ]
+
+
+view : Model -> Bool -> Maybe Int -> Html Msg
+view model selected number =
   let
     maybeYellow = if selected then (backgroundColor "yellow") else []
     clicker = onClick Click
+    clueNumber = case number of
+      Maybe.Just n -> span [ style numberStyle ] [ n |> toString |> text ]
+      Maybe.Nothing -> span [] []
   in
     case model of
-      Empty -> td [ clicker, style (letterBox ++ maybeYellow) ] []
-      Block -> td [ clicker, style (letterBox ++ (backgroundColor "black") ++ maybeYellow) ] []
-      Value c -> td [ clicker, style (letterBox ++ maybeYellow) ] [ text (String.fromChar c) ]
+      Empty -> td [ clicker, style (letterBox ++ maybeYellow) ] [ clueNumber ]
+      Block -> td [ clicker, style (letterBox ++ (backgroundColor "black") ++ maybeYellow) ] [ ]
+      Value c -> td [ clicker, style (letterBox ++ maybeYellow) ] [ clueNumber, text (String.fromChar c) ]
